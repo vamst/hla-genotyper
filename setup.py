e2fbea627f50f215111d6811a2722e9f95488f66
Vyacheslav Amstislavskiy <vamstis@gmail.com>
2023-05-05 10:52:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from codecs import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 execfile("hla/version.py")
